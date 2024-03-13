0f54975795c4e5281d9218bebf58943f6ca8e2ba
Rick Brunstedt <rbrunstedt@gmail.com>
2024-03-13 14:01:05 +0100

    add theme switcher plugin

---
+++ lua/custom/plugins/theme-switcher.lua
@@ -0,0 +1,56 @@
+-- local Telescope = require 'nvim-telescope/telescope.nvim'
+-- local themes = Teescope.themes = require('telescope.themes')
+--
+--
+-- local themes = require('telescope.themes'),
+-- print(themes)
+--
+-- local theme = Telescope.tokyonight.tokynight
+-- local dark = Telescope.tokyonight.tokynight.default
+-- local light = Telescope.tokyonight.tokynight.day
+--
+-- print(theme)
+-- print(dark)
+-- print(light)
+
+local dark_theme = 'tokyonight'
+local light_theme = 'tokyonight-day'
+Is_light_theme = false
+
+local function setTheme(light)
+  if light then
+    Is_light_theme = true
+    vim.cmd.colorscheme(light_theme)
+  else
+    Is_light_theme = false
+    vim.cmd.colorscheme(dark_theme)
+  end
+end
+
+local function setLightTheme()
+  setTheme(true)
+end
+
+local function setDarkTheme()
+  setTheme(false)
+end
+
+local function toggleTheme()
+  local newValue = not Is_light_theme
+  setTheme(newValue)
+end
+
+local function themeSwitcher()
+  vim.cmd 'Telescope colorscheme'
+end
+
+vim.keymap.set('n', '<leader>wtl', setLightTheme, { desc = '[L]ight theme' })
+vim.keymap.set('n', '<leader>wtd', setDarkTheme, { desc = '[D]ark theme' })
+vim.keymap.set('n', '<leader>wtt', toggleTheme, { desc = '[T]oggle theme' })
+vim.keymap.set('n', '<leader>wts', themeSwitcher, { desc = '[S]witch theme' })
+
+require('which-key').register {
+  ['<leader>wt'] = { name = '[T]themes', _ = 'which_key_ignore' },
+}
+
+return {}
