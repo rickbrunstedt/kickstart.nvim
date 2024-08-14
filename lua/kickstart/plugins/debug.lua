5f35e02fbd7a35e326edc9f1012325947b8cce20
Rick Brunstedt <rbrunstedt@gmail.com>
2024-08-14 09:22:56 +0200

    add nvim-nio

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -13,6 +13,7 @@ return {
   dependencies = {
     -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
+    'nvim-neotest/nvim-nio',
 
     -- Required dependency for nvim-dap-ui
     'nvim-neotest/nvim-nio',
