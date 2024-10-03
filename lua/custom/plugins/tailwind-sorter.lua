33b8fb8a13d8b389d3ddade0b19e7ba10b399453
Rick Brunstedt <rbrunstedt@gmail.com>
2024-10-03 16:25:32 +0200

    fix tailwind sorter plugin

--- lua/custom/plugins/tailwind-sorter.lua
+++ lua/custom/plugins/tailwind-sorter.lua
@@ -1,6 +1,9 @@
 return {
-  'laytan/tailwind-sorter.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
-  build = 'cd formatter && npm ci && npm run build',
-  config = true,
+    'laytan/tailwind-sorter.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
+    build = 'cd formatter && npm ci && npm run build',
+    opts = {
+        on_save_enabled = true, -- If `true`, automatically enables on save sorting.
+        trim_spaces = true,     -- If `true`, trim any extra spaces after sorting.
+    }
 }
