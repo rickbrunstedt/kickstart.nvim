73c05d23d33f404e2939b79f8777386fee18540e
Rick Brunstedt <rbrunstedt@gmail.com>
2024-10-03 16:24:48 +0200

    disable harpoon and ts-context-comment for now

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,34 +1,36 @@
-return {
-  'ThePrimeagen/harpoon',
-  config = function()
-    require('which-key').add {
-      { "<leader>1", group = "which_key_ignore" },
-      { "<leader>2", group = "which_key_ignore" },
-      { "<leader>3", group = "which_key_ignore" },
-      { "<leader>4", group = "which_key_ignore" },
-      { "<leader>a", group = "[A]dd harpoon mark" },
-      { "<leader>a_", hidden = true },
-      { "<leader>h", group = "[H]arpoon menu" },
-      { "<leader>h_", hidden = true },
-    }
+return {}
 
-    local mark = require 'harpoon.mark'
-    local ui = require 'harpoon.ui'
-
-    vim.keymap.set('n', '<leader>a', mark.add_file)
-    vim.keymap.set('n', '<leader>h', ui.toggle_quick_menu)
-
-    vim.keymap.set('n', '<leader>1', function()
-      ui.nav_file(1)
-    end)
-    vim.keymap.set('n', '<leader>2', function()
-      ui.nav_file(2)
-    end)
-    vim.keymap.set('n', '<leader>3', function()
-      ui.nav_file(3)
-    end)
-    vim.keymap.set('n', '<leader>4', function()
-      ui.nav_file(4)
-    end)
-  end,
-}
+-- return {
+--   'ThePrimeagen/harpoon',
+--   config = function()
+--     require('which-key').add {
+--       { '<leader>1', group = 'which_key_ignore' },
+--       { '<leader>2', group = 'which_key_ignore' },
+--       { '<leader>3', group = 'which_key_ignore' },
+--       { '<leader>4', group = 'which_key_ignore' },
+--       { '<leader>a', group = '[A]dd harpoon mark' },
+--       { '<leader>a_', hidden = true },
+--       { '<leader>h', group = '[H]arpoon menu' },
+--       { '<leader>h_', hidden = true },
+--     }
+--
+--     local mark = require 'harpoon.mark'
+--     local ui = require 'harpoon.ui'
+--
+--     vim.keymap.set('n', '<leader>a', mark.add_file)
+--     vim.keymap.set('n', '<leader>h', ui.toggle_quick_menu)
+--
+--     vim.keymap.set('n', '<leader>1', function()
+--       ui.nav_file(1)
+--     end)
+--     vim.keymap.set('n', '<leader>2', function()
+--       ui.nav_file(2)
+--     end)
+--     vim.keymap.set('n', '<leader>3', function()
+--       ui.nav_file(3)
+--     end)
+--     vim.keymap.set('n', '<leader>4', function()
+--       ui.nav_file(4)
+--     end)
+--   end,
+-- }
--- lua/custom/plugins/ts-context-comment.lua
+++ lua/custom/plugins/ts-context-comment.lua
@@ -1,14 +1,16 @@
+return {}
+
 -- vim.g.skip_ts_context_commentstring_module = true
 
-return {
-  'JoosepAlviste/nvim-ts-context-commentstring',
-  config = function()
-    require('Comment').setup {
-      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-    }
+-- return {
+--   'JoosepAlviste/nvim-ts-context-commentstring',
+--   config = function()
+--     require('Comment').setup {
+--       pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+--     }
 
-    require('ts_context_commentstring').setup {
-      -- enable_autocmd = false,
-    }
-  end,
-}
+--     require('ts_context_commentstring').setup {
+--       -- enable_autocmd = false,
+--     }
+--   end,
+-- }
